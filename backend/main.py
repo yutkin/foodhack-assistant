780ba76c1659f3f553d832a3d31ccf4d0db1e9bf
Dmitry Yutkin <yutkinn@gmail.com>
2018-03-03 20:06:04 +0300

    intent_classify

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,32 @@
 import json
 import os
 import glob
+import random
+import numpy as np
+from scipy.spatial.distance import cosine
 
-from flask import Flask, url_for, jsonify, g, redirect, send_from_directory
+from flask import Flask, jsonify,  Response
+from fastText import FastText
+
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 PATH_TO_RECIPES = './recipes'
+PATH_TO_FASTTEXT_MODEL = './wiki.ru.bin'
 RECIPES = []
+FASTTEXT = None
+
+INTENTS = {
+        'repeat_step': ('повтори рецепт', 'давай заново', 'прочитай снова', 'начни заново',
+                        'повторим', 'еще раз', 'прочитай еще раз'),
+        'step_next': ('дальше', 'я сделал', 'готово', 'давай дальше', 'следующий шаг',
+                      'закончили', 'шаг вперед'),
+        'step_back': ('назад', 'вернемся', "давай вернемся назад", 'шаг назад'),
+        'stop_cooking': ('стоп', "остановись", "перерыв", "давай остановимся",
+                         'перестань', 'хватит'),
+        'unknown': None
+    }
 
 
 def init_list_of_recipes():
@@ -17,18 +35,43 @@ def init_list_of_recipes():
         if os.path.isdir(recip_dir):
             for json_file in glob.glob(recip_dir+'/*.json'):
                 RECIPES.append(json.load(open(json_file, 'r')))
-    # print(json.dumps(RECIPES, indent=4, sort_keys=True, ensure_ascii=False))
+
+
+def init_fasttext():
+    global FASTTEXT
+    FASTTEXT = FastText.load_model(PATH_TO_FASTTEXT_MODEL)
+
+
+def get_closest_dist(text, list_of_examples):
+    return np.min([cosine(FASTTEXT.get_sentence_vector(text), FASTTEXT.get_sentence_vector(item))
+                   for item in list_of_examples])
+
+
+@app.route('/api/get_text_intent/<text>')
+def get_text_intent(text):
+    min_dist, intent = 1e9, None
+    for intent, examples in INTENTS.items():
+        d = get_closest_dist(text, examples)
+        if d < min_dist:
+            min_dist, intent = d, intent
+    if min_dist > 0.4:
+        intent = 'unknown'
+    return jsonify({'intent': intent})
 
 
 @app.route('/api/get_recipes')
 def get_recipes():
-    return jsonify({'recipes': RECIPES})
+    return Response(json.dumps({'recipes': RECIPES}, indent=2, sort_keys=True, ensure_ascii=False),
+                    content_type="application/json; charset=utf-8")
 
 
 def main():
     # APP.run(host='0.0.0.0', port=80, debug=False, threaded=True)
+
     init_list_of_recipes()
-    app.run(host='localhost', port=8888, debug=True, threaded=False)
+    # init_fasttext()
+
+    app.run(host='0.0.0.0', port=8888, debug=False, threaded=True)
 
 
 if __name__ == '__main__':
